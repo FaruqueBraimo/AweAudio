b5e3de66bfdc1ca64705d54a6ef49457a46ffcad
FaruqueBraimo <faruquebraimo@gmail.com>
2020-01-16 16:22:52 +0200

    #6 Audio das palavras
    Implementado com sucesso

--- quasar.conf.js
+++ quasar.conf.js
@@ -8,7 +8,7 @@ module.exports = function (ctx) {
     // https://quasar.dev/quasar-cli/cli-documentation/boot-files
     boot: [
        
-      'firebase'
+      'firebase', 'speech'
     ],
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
